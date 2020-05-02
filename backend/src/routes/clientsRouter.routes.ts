abfa5ad7cdf7edf3d83fee019081c7fe408cd845
luanfonsecap <luanfonsecap@icloud.com>
2020-05-02 15:34:36 -0300

    created route to update client data

--- backend/src/routes/clientsRouter.routes.ts
+++ backend/src/routes/clientsRouter.routes.ts
@@ -1,9 +1,31 @@
 import { Router, Request, Response } from 'express';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateClientDataService from '../services/UpdateClientDataService';
+import UpdateClientPasswordService from '../services/UpdateClientPasswordService';
 
 const clientsRouter = Router();
 
 clientsRouter.use(ensureAuthenticated);
+
+clientsRouter.put('/', async (req: Request, res: Response) => {
+	const { id } = req.user;
+	const { name, telephone, zipcode, street, number, city, uf } = req.body;
+
+	const updateClientDataService = new UpdateClientDataService();
+
+	await updateClientDataService.execute({
+		id,
+		name,
+		telephone,
+		zipcode,
+		street,
+		number,
+		city,
+		uf,
+	});
+
+	return res.status(204).send();
+});
 
 export default clientsRouter;
---
+++ backend/src/services/UpdateClientDataService.ts
@@ -0,0 +1,51 @@
+import { getRepository } from 'typeorm';
+
+import Client from '../models/Client';
+import AppError from '../errors/AppError';
+
+interface Request {
+	id: string;
+	name: string;
+	telephone: number;
+	zipcode: number;
+	street: string;
+	number: number;
+	city: string;
+	uf: string;
+}
+
+class UpdateClientDataService {
+	public async execute({
+		id,
+		name,
+		telephone,
+		zipcode,
+		street,
+		number,
+		city,
+		uf,
+	}: Request): Promise<void> {
+		const clientRepository = getRepository(Client);
+
+		let clientToUpdate = await clientRepository.findOne({ id });
+
+		if (!clientToUpdate) {
+			throw new AppError(404, 'Client not found with this id.');
+		}
+
+		clientToUpdate = {
+			...clientToUpdate,
+			name,
+			telephone,
+			zipcode,
+			street,
+			number,
+			city,
+			uf,
+		};
+
+		await clientRepository.save(clientToUpdate);
+	}
+}
+
+export default UpdateClientDataService;
