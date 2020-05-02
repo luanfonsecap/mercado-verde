563bfa3e400ceff2818fbe3839102eaf5dd75a1f
luanfonsecap <luanfonsecap@icloud.com>
2020-05-02 17:51:38 -0300

    change structure to acpetd tests

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import 'express-async-errors';
 import 'reflect-metadata';
-import './database';
 
+import createConnection from './database';
 import app from './app';
+
+createConnection();
 
 app.listen(3333, () => console.log('🔥️ Server started...'));
